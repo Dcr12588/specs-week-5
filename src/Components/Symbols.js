0f5e31a5136f434c49cce51e8f1dcfddb98a2237
Danny Ramirez <dcr1999jr@gmail.com>
2022-11-11 17:48:27 -0800

    help explaining overviewtable-class-inWeatherjs

--- src/Components/Symbols.js
+++ src/Components/Symbols.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useSelector } from "react-redux"
+import { selectDisplay } from '../redux/slices/displayCountrySlice'
 
 const Symbols = () => {
-    return <div>Symbols</div>
+    let currentDisplay = useSelector(selectDisplay)
+    return <div className='Symbols'>
+        <div className='Stack'>
+            <h2>Flag of: {currentDisplay.name.common}:</h2>
+            {currentDisplay.flags ? (
+                <img src = {currentDisplay.flags.png}/>
+            ) : (
+                '(No Data Found'
+            )}
+        </div>
+        <div className='Stack'>
+                <h2>Coat of Arms of {currentDisplay.name.common}:</h2>
+                {currentDisplay.coatOfArms ? (
+                    <img src = {currentDisplay.coatOfArms.png} />
+                ) : (
+                    '(No Data Found)'
+                )}
+        </div>
+    </div>
 };
 
 export default Symbols;
